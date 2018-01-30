5fd784844b715ab9b9b11dd801b96ed535448a69
Robert <rquinones93@yahoo.com>
2018-01-29 20:38:44 -0800

    The CodePlayer to the extent of the UDemy course works as intended. Copied some jQuery code because I'm really tired. Going to work on making it more unique now.

--- 4-jquery/codeRunnerJS.js
+++ 4-jquery/codeRunnerJS.js
@@ -1,15 +1,44 @@
-// jQuery Code
+/* jQuery */
 
-/**
- * On hover on of the toggleButton change the background color,
- * revert back on hover off.
- */
-$(".toggleButton").hover(function() {
-                
+$(".toggleButton").hover(function () {
+
   $(this).addClass("highlightedButton");
-  
-}, function() {
-  
+
+}, function () {
+
   $(this).removeClass("highlightedButton");
-  
+
 });
+
+$(".toggleButton").click(function () {
+  $(this).toggleClass("active");
+  $(this).removeClass("highlightedButton");
+  var panelId = $(this).attr("id") + "Panel";
+
+  $("#" + panelId).toggleClass("hidden");
+
+  var numberOfActivePanels = 4 - $('.hidden').length;
+
+  $(".panel").width(($(window).width() / numberOfActivePanels) - 5);
+});
+
+$(".panel").height($(window).height() - $("#topBar").height() - 15);
+
+$(".panel").width(($(window).width() / 2) - 10);
+
+$("textarea").on('change keyup paste', function () {
+  updateOutput();
+});
+
+/* Regular Javascript */
+function updateOutput() {
+
+  $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
+
+  document.getElementById("outputPanel").contentWindow.eval($("#jsPanel").val());
+
+
+
+}
+
+updateOutput();
